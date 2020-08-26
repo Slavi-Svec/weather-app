dc8c5879f33a597ce6fcb40cb6d509550777080f
izaak svec <slavi.svec@gmail.com>
2020-08-26 19:07:35 +1000

    Corrected indentation

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const seeLocation = document.getElementsByClassName('location')[0]
 const seeWeather = document.getElementsByClassName('weather-out')[0]
 const seeIcon = document.getElementsByClassName('weather-icon')[0]
 
-
-
 const updateWeather = () => {
   const fetchTheUrl =  'http://api.openweathermap.org/data/2.5/weather?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
   fetch(fetchTheUrl)
