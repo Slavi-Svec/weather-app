8492c048371ad2d5a9b41b944fb4723bc237b648
izaak svec <slavi.svec@gmail.com>
2020-08-28 08:55:58 +1000

    added temperature logo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const locationDiv = document.getElementsByClassName('location')[0]
 const weatherDiv = document.getElementsByClassName('weather-out')[0]
-const iconDiv = document.getElementsByClassName('weather-icon')[0]
+const iconImg = document.getElementsByClassName('weather-icon')[0]
 
 const updateWeather = () => {
   const url =  'http://api.openweathermap.org/data/2.5/weather?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
@@ -11,14 +11,20 @@ const updateWeather = () => {
     .then((weather) => {
       const location = weather.name
       const temp = weather.main.temp
-      const icon = weather.weather[0].icon
+      const iconCode = weather.weather[0].icon
+      const iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
 
       locationDiv.textContent = location
       weatherDiv.textContent = temp
-      iconDiv.textContent = icon
+      iconImg.src = iconUrl
       console.log(temp)
-      console.log(icon)
+      console.log(iconCode)
     })
 }
 
 updateWeather()
+
+
+// "http://openweathermap.org/img/w/02d.png"
+
+// document["pic1"].src = searchPic.src;
