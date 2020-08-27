d2c83098d6605afbe808d8b942005a74f36e6043
izaak svec <slavi.svec@gmail.com>
2020-08-28 08:09:55 +1000

    fixed review changes

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const seeLocation = document.getElementsByClassName('location')[0]
-const seeWeather = document.getElementsByClassName('weather-out')[0]
-const seeIcon = document.getElementsByClassName('weather-icon')[0]
+const locationDiv = document.getElementsByClassName('location')[0]
+const weatherDiv = document.getElementsByClassName('weather-out')[0]
+const iconDiv = document.getElementsByClassName('weather-icon')[0]
 
 const updateWeather = () => {
-  const fetchTheUrl =  'http://api.openweathermap.org/data/2.5/weather?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
-  fetch(fetchTheUrl)
+  const url =  'http://api.openweathermap.org/data/2.5/weather?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
+  fetch(url)
     .then((response) => {
       return response.json()
     })
-    .then((data) => {
-      const getLocation = data.name
-      seeLocation.textContent = getLocation
-      const getTemp = data.main.temp
-      seeWeather.textContent = getTemp
-      console.log(getTemp)
-      const icon = data.weather[0].icon
-      seeIcon.textContent = icon
+    .then((weather) => {
+      const location = weather.name
+      const temp = weather.main.temp
+      const icon = weather.weather[0].icon
+
+      locationDiv.textContent = location
+      weatherDiv.textContent = temp
+      iconDiv.textContent = icon
+      console.log(temp)
       console.log(icon)
     })
-  }
+}
 
-console.log(updateWeather())
+updateWeather()
