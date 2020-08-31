789a9e90235879e6c0c320848afb36d9034c2895
izaak svec <slavi.svec@gmail.com>
2020-08-31 18:37:16 +1000

    Updated console.log for 5 days

--- script.js
+++ script.js
@@ -3,21 +3,36 @@ const weatherDiv = document.getElementsByClassName('weather-out')[0]
 const iconImg = document.getElementsByClassName('weather-icon')[0]
 
 const updateWeather = () => {
-  const url =  'http://api.openweathermap.org/data/2.5/weather?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
+  const url =  'http://api.openweathermap.org/data/2.5/forecast?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
+
   fetch(url)
     .then((response) => response.json())
     .then((weather) => {
-      const location = weather.name
-      const temp = weather.main.temp
-      const iconCode = weather.weather[0].icon
-      const iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
+      // const weatherDayOne = weather.list
+      const list = weather.list
 
-      locationDiv.textContent = location
-      weatherDiv.textContent = temp
-      iconImg.src = iconUrl
-      console.log(temp)
-      console.log(iconCode)
+      for (let i = 0; i < list.length; i = i + 8) {
+        console.log(list[i])
+      }
+      // const location = weather.name
+      // const temp = Math.round(weather.main.temp)
+      // const tempMin = weather.main.temp_min
+      // const tempMax = weather.main.temp_max
+      // const iconCode = weather.weather[0].icon
+      // const iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
+
+
+      // locationDiv.textContent = location
+      // weatherDiv.textContent = temp
+      // iconImg.src = iconUrl
+
+      // console.log(temp)
+      // console.log(iconCode)
+      // console.log(tempMin, tempMax)
+      // console.log(weather)
+      // console.log(weatherDayOne)
     })
 }
 
 updateWeather()
+
