34a5a4f8146d4dfd3e6acc691e8357fc114b9f91
izaak svec <slavi.svec@gmail.com>
2020-09-04 09:01:12 +1000

    Updated style of day and temp

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-const locationDiv = document.getElementsByClassName('location')[0]
-const weatherDiv = document.getElementsByClassName('weather-out')[0]
-const iconImg = document.getElementsByClassName('weather-icon')[0]
+const fragment = document.createDocumentFragment()
+const weatherDiv  = document.getElementsByClassName('weather')[0]
+const minDiv = document.getElementsByClassName('min')[0]
+const maxDiv = document.getElementsByClassName('max')[0]
+const iconDiv = document.getElementsByClassName('icon')[0]
+const dayOfWeekDiv = document.getElementsByClassName('weekDay')[0]
 
-const updateWeather = () => {
-  const url =  'http://api.openweathermap.org/data/2.5/forecast?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
+const updateWeather = (url) => {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
   fetch(url)
     .then((response) => response.json())
     .then((weather) => {
-      // const weatherDayOne = weather.list
       const list = weather.list
 
       for (let i = 0; i < list.length; i = i + 8) {
-        console.log(list[i])
+        let min = Math.round(list[i].main.temp_min)
+        let max = Math.round(list[i].main.temp_max)
+
+        const iconCode = list[i].weather[0].icon
+        const date = new Date(list[i].dt * 1000);
+        const dayOfWeek = days[date.getDay()]
+        const iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
+
+        iconDiv.src = iconUrl
+        minDiv.innerText = min
+        maxDiv.innerText = max
+        dayOfWeekDiv.innerText = dayOfWeek
       }
-      // const location = weather.name
-      // const temp = Math.round(weather.main.temp)
-      // const tempMin = weather.main.temp_min
-      // const tempMax = weather.main.temp_max
-      // const iconCode = weather.weather[0].icon
-      // const iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'
-
-
-      // locationDiv.textContent = location
-      // weatherDiv.textContent = temp
-      // iconImg.src = iconUrl
-
-      // console.log(temp)
-      // console.log(iconCode)
-      // console.log(tempMin, tempMax)
-      // console.log(weather)
-      // console.log(weatherDayOne)
     })
 }
 
-updateWeather()
+updateWeather('http://api.openweathermap.org/data/2.5/forecast?zip=2000,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa')
 
+        // weatherDiv.innerText = min
+
+        // console.log(weather)
+
+        // const weatherFiveDay = [min, max]
+
+        // weatherFiveDay.forEach(function(weatherFiveDay) {
+        // let li = document.createElement('li')
+        // li.textContent = weatherFiveDay
+        // fragment.appendChild(li)
+        // })
+
+        // fiveDayDiv.appendChild(fragment)
